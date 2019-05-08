950bcfea777641cadd516572d2975ff0ed80c7d3
BryanPrintz <BryanPrintz@localhost>
2019-05-08 16:36:51 +0200

    Camera fixed, Skybox added

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Transform lookAt = null;
+    [SerializeField] private Camera cam;
+    
     private CharacterController _controller;
     
     public float speed = 6.0f;
     public float jumpDuration = 8.0f;
     public float gravity = 20.0f;
 
-    private Vector3 _moveDirection = Vector3.zero;
+    private Vector3 _moveDirection = new Vector3(0,0,0);
     
     private void Start()
     {
@@ -22,6 +25,15 @@ public class PlayerMovement : MonoBehaviour
         if (_controller.isGrounded)
         {
             _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+            var lookAtPoint = transform.position - _moveDirection;
+            _moveDirection = cam.transform.TransformDirection(_moveDirection);
+            
+            
+            
+            
+            transform.LookAt(lookAtPoint);
+            
             _moveDirection *= speed;
 
             if (Input.GetButton("Jump"))
