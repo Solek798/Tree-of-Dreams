940ae2c37a7f96bfd5776143ee080dc2670e64cf
FKonprecht <FKonprecht@localhost>
2019-05-09 01:35:28 +0200

    Added FarmlandLevel Behaviour to Lcok/Unlock Cells

---
+++ Assets/FarmlandSystem/FarmlandLevel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmlandLevel : MonoBehaviour
+{
+    private HashSet<Vector3Int> _lockedCells;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lockedCells = new HashSet<Vector3Int>();
+    }
+
+    public void LockCell(Vector3Int cellToLock)
+    {
+        _lockedCells.Add(cellToLock);
+    }
+
+    public void UnlockCell(Vector3Int cellToUnlock)
+    {
+        _lockedCells.Remove(cellToUnlock);
+    }
+
+    public bool IsLocked(Vector3Int cell)
+    {
+        return _lockedCells.Contains(cell);
+    }
+}
