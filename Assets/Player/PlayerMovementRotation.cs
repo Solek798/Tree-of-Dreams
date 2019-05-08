44c6e0a10038cbbb5453ff167b0bad26685dc044
BryanPrintz <BryanPrintz@localhost>
2019-05-08 16:36:51 +0200

    Camera fixed, Skybox added

---
+++ Assets/Player/PlayerMovementRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementRotation : MonoBehaviour
+{
+    [SerializeField] private Transform lookAt = null;
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.rotation(lookAt);
+    }
+}
