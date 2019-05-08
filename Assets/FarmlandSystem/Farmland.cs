7ab997a4c595f918f3299c9a949c584e87fd8f67
FKonprecht <FKonprecht@localhost>
2019-05-08 17:24:22 +0200

    Added Core of FarmlandSystem
    - The Farmland consists out of several Farmland Levels
    - Each Level has a Tilemap
    - The green plantable Tiles define where Plants can be planted

---
+++ Assets/FarmlandSystem/Farmland.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Farmland : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var map in GetComponentsInChildren<Tilemap>())
+        {
+            map.color = Color.clear;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
