d6e018a4444a7c05d080126f4c3003868d023011
BryanPrintz <BryanPrintz@localhost>
2019-05-13 15:40:16 +0200

    Added Plant stages and made the plants a scriptable Object. Also imported some assets from the Milestone 00 Version of Dreamtree as a placeholder

---
+++ Assets/Environment/Plants/PlantScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlantObject", menuName = "ScriptableObjects/Plants", order = 1)]
+public class PlantScriptableObject : ScriptableObject
+{
+    public string Name;
+    public List<int> StateReachTime = new List<int>();
+    public List<GameObject> StateModel = new List<GameObject>();
+
+}
