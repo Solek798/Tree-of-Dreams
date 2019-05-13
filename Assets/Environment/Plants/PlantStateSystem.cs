6336701c63f49f317910e9c8b0220680b9301ebf
BryanPrintz <BryanPrintz@localhost>
2019-05-13 15:47:10 +0200

    Made the code more compact and round and shortened it.

--- Assets/Environment/Plants/PlantStateSystem.cs
+++ Assets/Environment/Plants/PlantStateSystem.cs
@@ -7,34 +7,31 @@ public class PlantStateSystem : MonoBehaviour
 {
     public PlantScriptableObject PlantObject;
     public int currentState = 0;
-    
-    
+
     private void Update()
     {
         //var CurrentModel = PlantObject.StateModel;
-        if (Input.GetButtonDown("Fire2"))
+        if (!Input.GetButtonDown("Fire2")) return;
+        if (currentState < PlantObject.StateModel.Count)
         {
-            if (currentState < PlantObject.StateModel.Count)
-            {
-                UpdateCurrentState();
-            }
-            
+            UpdateCurrentState();
         }
-        
     }
-    
+
     private void UpdateCurrentState()
     {
         GameObject newPlantModel = PlantObject.StateModel[currentState];
         currentState += 1;
-        if(transform.GetChild(0).childCount > 0)
+
+        if (transform.GetChild(0).childCount > 0)
         {
-            foreach(Transform GO in transform.GetChild(0))
+            foreach (Transform GO in transform.GetChild(0))
             {
                 Destroy(GO.gameObject);
             }
         }
 
-        GameObject PlantModel = Instantiate(newPlantModel, transform.GetChild(0).transform.position,Quaternion.identity, transform.GetChild(0));
+        GameObject PlantModel = Instantiate(newPlantModel, transform.GetChild(0).transform.position,
+            Quaternion.identity, transform.GetChild(0));
     }
 }
