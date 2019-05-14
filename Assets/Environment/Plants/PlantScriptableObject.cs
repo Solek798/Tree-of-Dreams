87e043327852fc06110e01e5efb845f23a5d7f7a
BryanPrintz <BryanPrintz@localhost>
2019-05-14 10:55:06 +0200

    Added Shadergraph Commented out some Scripts

--- Assets/Environment/Plants/PlantScriptableObject.cs
+++ Assets/Environment/Plants/PlantScriptableObject.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlantObject", menuName = "ScriptableObjects/Plants", order = 1)]
 public class PlantScriptableObject : ScriptableObject
 {
+    //Plantname
     public string Name;
+    //The Time the Plant needs to grow
     public List<int> StateReachTime = new List<int>();
+    //The Model of the specific State
     public List<GameObject> StateModel = new List<GameObject>();
 
 }
