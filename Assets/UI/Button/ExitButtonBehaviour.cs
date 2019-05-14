2c32a92368de442360c8797293270cf44439b253
BryanPrintz <BryanPrintz@localhost>
2019-05-14 17:05:14 +0200

    Main menu placeholder added (Play and Exit Button working)

---
+++ Assets/UI/Button/ExitButtonBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonBehaviour : MonoBehaviour
+{
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
