e3bb5baa60b8789dc3b43d7b637e713ddd1e90b5
BryanPrintz <BryanPrintz@localhost>
2019-05-15 16:16:37 +0200

    Added Nightcycleish system to the Dreamtree (Sleeping.cs) also removed the update function from PlantState.cs

---
+++ Assets/Environment/DreamTree/Sleeping.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sleeping : MonoBehaviour
+{
+    public GameObject player;
+    public float maxDistanceToSleep = 10f;
+
+
+    private void Update()
+    {
+        var plants = GameObject.FindGameObjectsWithTag("Plant");
+
+
+        var playerInRange = Vector3.Distance(transform.position, player.transform.position);
+
+        if (!Input.GetButtonDown("Fire2") || !(playerInRange <= maxDistanceToSleep)) return;
+        foreach (var plant in plants)
+        {
+            plant.GetComponent<PlantState>().UpdateCurrentState();
+        }
+
+        StartCoroutine(Wait());
+    }
+
+    private void FadeBlack()
+    {
+        var image = GetComponentInChildren<Image>();
+        var tempColor = image.color;
+        tempColor.a = 255f;
+        image.color = tempColor;
+    }
+
+    private IEnumerator Wait()
+    {
+        FadeBlack();
+
+        yield return new WaitForSeconds(1);
+
+        FadeTransparent();
+    }
+
+    private void FadeTransparent()
+    {
+        var image = GetComponentInChildren<Image>();
+        var tempColor = image.color;
+        tempColor.a = 0f;
+        image.color = tempColor;
+    }
+}
--- Assets/Environment/Plants/PlantState.cs
+++ Assets/Environment/Plants/PlantState.cs
@@ -14,33 +14,22 @@ public class PlantState : MonoBehaviour
         Instantiate(plantObject.StateModel[currentState], transform.position, Quaternion.identity, transform);
     }
 
-    private void Update()
-    {
-        //TODO: Implement this one to the Day n Night Cycle and remove the Button which is only for Debugging
-        //Fire2 = left Alt key
-        if (!Input.GetButtonDown("Fire2")) return;
-        if (currentState < plantObject.StateModel.Count)
-        {
-            UpdateCurrentState();
-        }
-    }
-
     //Exchanges the current Model with the next Model in the list of the Scriptable Object
-    private void UpdateCurrentState()
+    public void UpdateCurrentState()
     {
         if (currentState >= plantObject.StateModel.Count - 1)
             return;
-        
+
         currentState++;
         GameObject newPlantModel = plantObject.StateModel[currentState];
-        
-        
+
+
         foreach (var childTransform in GetComponentsInChildren<Transform>())
         {
-            if (childTransform != transform) 
+            if (childTransform != transform)
                 Destroy(childTransform.gameObject);
         }
-        
+
         GameObject PlantModel = Instantiate(newPlantModel, transform.position,
             Quaternion.identity, transform);
     }
