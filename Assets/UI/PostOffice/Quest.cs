dde0a00148e4563b2015f5cc45110ada622165a7
BryanPrintz <BryanPrintz@localhost>
2019-05-28 11:35:27 +0200

    Started to build the Ui for the Quesystem

---
+++ Assets/UI/PostOffice/Quest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "QuestObject", menuName = "ScriptableObjects/Quest", order = 2)]
+public class Quest : ScriptableObject
+{
+    //Questname
+    public new string name;
+
+    public string questDescription;
+
+    //Requirements
+    public List<GameObject> requirements = new List<GameObject>();
+}
