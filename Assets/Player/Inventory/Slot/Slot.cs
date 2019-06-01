a4a2b2c59452738b1d46db80db3d3cd4bc4117b2
FKonprecht <FKonprecht@localhost>
2019-06-02 01:02:55 +0200

    Added Drag 'n' drop functionality
    - new UI Dragger Prefab needed for drag 'n' drop to work

--- Assets/Player/Inventory/Slot/Slot.cs
+++ Assets/Player/Inventory/Slot/Slot.cs
@@ -5,15 +5,21 @@ using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
-    [SerializeField] private Image stackIcon;
     private Stack<InventoryItem> _items;
-    private Text _stackCount;
+    private Info _info;
 
 
     private void Start()
     {
         _items = new Stack<InventoryItem>();
-        _stackCount = GetComponentInChildren<Text>();
+        _info = GetComponentInChildren<Info>();
+    }
+    
+    public void Swap(Slot other)
+    {
+        var otherStack = other._items;
+        other._items = this._items;
+        this._items = otherStack;
     }
 
     public bool Push(InventoryItem item)
@@ -22,22 +28,27 @@ public class Slot : MonoBehaviour
             return false;
         
         _items.Push(item);
-        stackIcon.sprite = item.Icon;
         
-        UpdateCount();
+        _info.Refresh(_items.Count, item.Icon);
         
         return true;
     }
-
+    
     public InventoryItem Pop()
     {
         var retVal = _items.Pop();
-        UpdateCount();
+        _info.Refresh(_items.Count);
         return retVal;
     }
 
-    private void UpdateCount()
+    public InventoryItem[] PopAll()
     {
-        _stackCount.text = _items.Count <= 1 ? "" : _items.Count.ToString();
+        var retVal = _items.ToArray();
+        _items.Clear();
+        
+        _info.Refresh(_items.Count);
+
+        return retVal;
     }
+    
 }
---
+++ Assets/UI/UIDragger.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIDragger : MonoBehaviour
+{
+
+    private bool _isDragging = false;
+    private GameObject _draggingObject;
+    private Transform _origPos;
+    private List<RaycastResult> _results;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _results = new List<RaycastResult>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_isDragging)
+        {
+            _draggingObject.transform.position = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _draggingObject = GetDragableObject();
+            print(_draggingObject);
+
+            if (_draggingObject != null)
+            {
+                _origPos = _draggingObject.transform.parent;
+                _draggingObject.transform.parent = _draggingObject.GetComponentInParent<Canvas>().transform;
+                _isDragging = true;
+            }  
+        }
+
+        if (Input.GetMouseButtonUp(0) && _isDragging)
+        {
+            _draggingObject.transform.parent = _origPos;
+            _draggingObject.transform.localPosition = Vector2.zero;
+            _isDragging = false;
+        }
+    }
+
+    private GameObject GetDragableObject()
+    {
+        var pointer = new PointerEventData(EventSystem.current) {position = Input.mousePosition};
+
+        EventSystem.current.RaycastAll(pointer, _results);
+
+        if (_results.Count == 0)
+            return null;
+        
+        return _results
+                .Select(t => t.gameObject)
+                .FirstOrDefault(t => t.CompareTag("UIDragable"));
+    }
+}
