13e0e895d310a5f06b2ce66683347e3ed6950635
BryanPrintz <BryanPrintz@localhost>
2019-06-03 17:37:06 +0200

    Now adds a Panel foreach Element in Requirements

--- Assets/Quests/Lampion/Lampion.cs
+++ Assets/Quests/Lampion/Lampion.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 using Image = UnityEngine.UIElements.Image;
 
@@ -10,34 +11,65 @@ public class Lampion : MonoBehaviour
     public Quest quest;
     public GameObject ui;
 
+    private int _requirementLength = 0;
     [SerializeField] private GameObject npcImage = null;
-    
+    [SerializeField] private GameObject uiRequirements = null;
+    [SerializeField] private GameObject uiPanel = null;
+
+    public bool uiOpened;
     
     private void Start()
     {
-        
-
-        
+        uiOpened = false;
+        _requirementLength = quest.requirements.Count;
     }
 
+    
+    void Parent( GameObject parentOb, GameObject childOb )
+    {
+        childOb.transform.parent = parentOb.transform;
+        childOb.transform.localScale = new Vector3(1, 1, 1);
+    }
+    
 
     private void LampionActivation()
     {
         //Get Data of the Scriptable Object
         ui.GetComponentInChildren<Text>().text = quest.questDescription;
         npcImage.GetComponent<UnityEngine.UI.Image>().sprite = quest.questNPCImage;
+
+
+        foreach (var value in quest.requirements)
+        {
+            var panelVariant = Instantiate(uiPanel);
+            Parent(uiRequirements,panelVariant);
+        }
         
-        
+
+
         ui.GetComponent<Canvas>().enabled = true;
+        uiOpened = true;
     }
 
     private void Update()
     {
         var playerInRange = Vector3.Distance(transform.position, player.transform.position);
 
-        if (Input.GetKeyDown(KeyCode.E) && playerInRange <= maxDistanceToPlayer)
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange <= maxDistanceToPlayer && uiOpened == false) 
         {
             LampionActivation();
+        }
+
+
+        if (ui.GetComponent<Canvas>().enabled == false)
+        {
+            uiOpened = false;
+            
+
+            foreach (Transform child in uiRequirements.transform) 
+            {
+                GameObject.Destroy(child.gameObject);
+            }
         }
     }
 }
