f31a1592cfa66c716ac533f0a984a8a3b2c591bb
BryanPrintz <BryanPrintz@localhost>
2019-06-03 12:16:49 +0200

    Started to work on the Quest system
    -Made a new mainfolder for Quests
    -Created the scriptable quest object
    -moved some files from post office to quests

---
+++ Assets/Quests/Lampion/Lampion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Lampion : MonoBehaviour
+{
+    public GameObject player;
+    public float maxDistanceToPlayer = 10f;
+    public Quest quest;
+    public GameObject ui;
+
+    private string _text;
+
+    private void Start()
+    {
+        _text = quest.questDescription;
+        //ui.GetComponentInChildren<Image>() = quest.questNPCImage;
+        
+    }
+
+
+    private void LampionActivation()
+    {
+        ui.GetComponent<Canvas>().enabled = true;
+    }
+
+    private void Update()
+    {
+        var playerInRange = Vector3.Distance(transform.position, player.transform.position);
+
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange <= maxDistanceToPlayer)
+        {
+            LampionActivation();
+        }
+    }
+}
