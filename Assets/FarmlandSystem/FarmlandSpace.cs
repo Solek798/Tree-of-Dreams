f2815b08392755e54d820ee64cae0448e9458936
FKonprecht <39970051+Solek798@users.noreply.github.com>
2019-06-17 09:52:13 +0200

    Boilerplated Tools Mechanic
    - Added FarmlandSpace which saves the state of the Farmtile

---
+++ Assets/FarmlandSystem/FarmlandSpace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmlandSpace : MonoBehaviour
+{
+    [SerializeField] private GameObject soil;
+    private Vector3Int _cell;
+    private bool _isNurtured;
+
+    public bool IsSoil
+    {
+        get => soil.activeInHierarchy;
+        set => soil.SetActive(value);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player/Tools/SeedFlute/SeedFlute.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedFlute : MonoBehaviour, ITool
+{
+    public bool Use()
+    {
+        Plant();
+    }
+
+    public bool IsUsable(FarmlandSpace space)
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    
+    private void Plant(FarmlandLevel level, GameObject plant, Vector3Int cell)
+    {
+
+        Vector3 position = level.GetWorldCord(cell);
+        
+        level.LockCell(cell);
+
+        var newPlant = Instantiate(plant, position, level.transform.rotation);
+        
+    }
+}
