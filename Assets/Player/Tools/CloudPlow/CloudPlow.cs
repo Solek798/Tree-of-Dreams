ccb0ac79d35a4a2649cf637f5431235a14e03ec4
FKonprecht <39970051+Solek798@users.noreply.github.com>
2019-06-24 02:22:46 +0200

    made Tool Usable with Inventory
    - All Tools now get placed in Inventory at the Start of the game
    - implemented BagOf Stardust

--- Assets/Player/Tools/CloudPlow/CloudPlow.cs
+++ Assets/Player/Tools/CloudPlow/CloudPlow.cs
@@ -8,14 +8,11 @@ public class CloudPlow : MonoBehaviour, ITool
     
     public bool Use(FarmlandSpace space)
     {
-        Debug.Log("Plowing");
         return space.IsSoil = true;
     }
 
     public bool IsUsable(FarmlandSpace space, Vector3 usagePoint)
     {
-        Debug.Log(space.transform.position);
-        Debug.Log(usagePoint);
         return (space.transform.position - usagePoint).sqrMagnitude <= maxPlowDistance;
     }
 }
