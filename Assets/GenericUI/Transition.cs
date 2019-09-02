8d939980483afd66681761c39b5901756e4acff3
FKonprecht <FKonprecht@localhost>
2019-09-02 02:20:11 +0200

    First implementation of the EndOfDayScreen into the Game
    - Screen is showing up when going to sleep
    - Screen is disappearing when clicking the continue button
    - Added Transition to the game

---
+++ Assets/GenericUI/Transition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public static Transition Instance { get; private set; }
+
+    public float FadeBlackTime => GetComponent<Animation>().GetClip("FadeBlack").length;
+    public float FadeNormalTime => GetComponent<Animation>().GetClip("FadeNormal").length;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void FadeBlack()
+    {
+        GetComponent<Animation>().CrossFade("FadeBlack");
+        
+    }
+
+    public void FadeNormal()
+    {
+        GetComponent<Animation>().CrossFade("FadeNormal");
+    }
+}
