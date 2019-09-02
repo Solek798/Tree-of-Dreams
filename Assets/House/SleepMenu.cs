c193c49211958fd7103f35fee93a00933f04486d
FKonprecht <39970051+Solek798@users.noreply.github.com>
2019-09-02 15:08:55 +0200

    work in progress commit

--- Assets/House/SleepMenu.cs
+++ Assets/House/SleepMenu.cs
@@ -1,13 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SleepMenu : MonoBehaviour
 {
+    [SerializeField] private QuestCollector questCollector = null;
+    [SerializeField] private Transform questLayoutGroup = null;
+    [SerializeField] private Text todaysEaringsText = null;
+
+    public int TodaysEarings
+    {
+        get => Convert.ToInt32(todaysEaringsText.text);
+        set => value.ToString();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (var quest in questCollector.GetAllQuests())
+        {
+            
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Quests/Display/ProgressDisplay.cs
@@ -0,0 +1,16 @@
+
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressDisplay : QuestDisplay
+{
+    [SerializeField] private Text fullfillText = null;
+    public override void Initialize(Quest quest)
+    {
+        _quest = quest;
+        npcIconUI.sprite = quest.Data.questNPCImage;
+        
+        
+    }
+}
