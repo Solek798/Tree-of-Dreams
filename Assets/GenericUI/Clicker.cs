b55581a973b08e3f38ba6ea083e5080a5b5f8c7c
Cornelius <Cornelius@localhost>
2019-09-05 00:27:17 +0200

    created clicker prefab added button click sound

---
+++ Assets/GenericUI/Clicker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Clicker : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioPlayer;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(IsMouseOverButton());
+        if (Input.GetMouseButtonUp(0) && IsMouseOverButton())
+        {    
+            audioPlayer.Play();
+        }
+    }
+
+    private bool IsMouseOverButton()
+    {
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        pointerEventData.position = Input.mousePosition;
+        
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+        
+        Debug.Log(raycastResults.Count);
+        
+        for (int i = 0; i < raycastResults.Count; i++)
+        {
+            if (raycastResults[i].gameObject.GetComponent<Button>() != null)
+            {
+                Debug.Log("ein button");
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
