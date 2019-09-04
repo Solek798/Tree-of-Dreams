c31d4374d009f9dcbcc28adee2e486c24b8fcfcf
Cornelius Nitschkoff <Cornelius Nitschkoff@localhost>
2019-09-04 19:18:19 +0200

    started clicker sound script

---
+++ Assets/Environment/Clicker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clicker : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioPlayer;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        ray.
+        
+        if ()
+        {
+            
+        }
+    }
+}
