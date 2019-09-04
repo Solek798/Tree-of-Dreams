8abe0e74bb64f19b64c320922b317578c6930737
FKonprecht <39970051+Solek798@users.noreply.github.com>
2019-09-04 23:36:14 +0200

    implemented Credits and Options to Main Menu

--- Assets/Journal/Journal.cs
+++ Assets/Journal/Journal.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Journal : MonoBehaviour
 {
     public Scrollbar slider = null;
---
+++ Assets/Scenes/MainMenu/MenuContent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuContent : MonoBehaviour
+{
+    [SerializeField] private Animation optionsAnimation = null;
+    [SerializeField] private Animation creditsAnimation = null;
+
+    private bool creditsOpened = false;
+    private bool optionsOpened = false;
+
+    public void OpenOptions()
+    {
+        if (optionsOpened)
+        {
+            optionsAnimation.Play("OptionsPopDown");
+            optionsOpened = false;
+        }
+        else
+        {
+            optionsAnimation.Play("OptionsPopUp");
+            optionsOpened = true;
+        }
+
+        if (creditsOpened)
+        {
+            creditsAnimation.Play("CreditsPopDown");
+            creditsOpened = false;
+        }
+    }
+
+    public void OpenCredits()
+    {
+        if (creditsOpened)
+        {
+            creditsAnimation.Play("CreditsPopDown");
+            creditsOpened = false;
+        } 
+        else
+        {
+            creditsAnimation.Play("CreditsPopUp");
+            creditsOpened = true;
+        }
+
+        if (optionsOpened)
+        {
+            optionsAnimation.Play("OptionsPopDown");
+            optionsOpened = false;
+        }
+    }
+}
