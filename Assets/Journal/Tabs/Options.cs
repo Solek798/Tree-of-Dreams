7d944b4131d2bf7b427f8f7592e71792af9cbf2d
FKonprecht <FKonprecht@localhost>
2019-09-04 23:36:14 +0200

    implemented Credits and Options to Main Menu

--- Assets/Journal/Tabs/Options.cs
+++ Assets/Journal/Tabs/Options.cs
@@ -1,18 +1,33 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Options : MonoBehaviour
 {
     [SerializeField] private AudioMixer mixer = null;
     [SerializeField] private GameObject ambientVFXSakura = null;
     [SerializeField] private GameObject ambientVFXGlow = null;
+    [SerializeField] private Slider sfxSlider = null;
+    [SerializeField] private Slider musicSlider = null;
+    [SerializeField] private Toggle ambientToggle = null;
     
-    
-    // Start is called before the first frame update
+    private static bool AmbientVFX { get; set; }
+
+
+    private void Start()
+    {
+        ToggleAmbiantVFX(AmbientVFX);
+        ambientToggle.isOn = AmbientVFX;
+
+        mixer.GetFloat("SFX", out var sfxValue);
+        mixer.GetFloat("Music", out var musicValue);
+
+        if (sfxSlider == null || musicSlider == null) return;
+        sfxSlider.value = sfxValue;
+        musicSlider.value = musicValue;
+    }
+
     public void ToggleFullscreen(bool value)
     {
         Screen.fullScreen = value;
@@ -20,8 +35,9 @@ public class Options : MonoBehaviour
 
     public void ToggleAmbiantVFX(bool value)
     {
-        ambientVFXGlow.SetActive(value);
-        ambientVFXSakura.SetActive(value);
+        AmbientVFX = value;
+        ambientVFXGlow?.SetActive(value);
+        ambientVFXSakura?.SetActive(value);
     }
 
     public void SetSFX(Single value)
