334435ff40e9c3ca2be6d3e11733711d7b386b33
FKonprecht <FKonprecht@localhost>
2019-09-04 13:30:07 +0200

    added Lampion Factory (again)

---
+++ Assets/Quests/Lampion/LampionFactory.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampionFactory : MonoBehaviour
+{
+    [SerializeField] private GameObject lampionPrefab = null;
+    [SerializeField] private GameObject player = null;
+    public Farmland farmland;
+
+    [SerializeField] private BoundsInt targetBounds;
+    [SerializeField] private float spawnRadius = 30;
+    [SerializeField] private int maxGeneratingAttempts = 10;
+    public List<QuestData> questData;
+
+
+    private void Start()
+    {
+        Debug.Log(farmland);
+    }
+    public void CreateAndSend()
+    {
+        if (questData.Count <= 0) return;
+
+        // Select random FarmlandSpace
+        Debug.Log("dAT fARMLAND: " + farmland);
+        //farmland = FindObjectOfType<Farmland>();
+        var levels = farmland.GetAllLevels();
+        var targetLevel = levels[Random.Range(0, levels.Length - 1)];
+        var targetSpace = targetLevel.GetRandomSpace(maxGeneratingAttempts);
+        Debug.Log("Dat Space: " + targetSpace);
+        if (targetSpace == null) return;
+
+        var newLampion = Create();
+        
+        Send(newLampion, targetSpace);
+    }
+
+    private Lampion Create()
+    {
+        var newLampion = Instantiate(lampionPrefab, transform).GetComponent<Lampion>();
+        newLampion.player = player;
+        
+        newLampion.questData = questData[Random.Range(0, questData.Count - 1)];
+        questData.Remove(newLampion.questData);
+
+        return newLampion;
+    }
+
+    private void Send(Lampion newLampion, FarmlandSpace targetSpace)
+    {
+        // Select random spawn point
+        Debug.Log(newLampion);
+        newLampion.transform.localPosition = new Vector3(spawnRadius * Random.value, 0, 0);
+        newLampion.transform.RotateAround(
+            newLampion.transform.parent.position, 
+            Vector3.up, 
+            360 * Random.value
+        );
+        
+        // send Lampion by setting TravelTarget
+        targetSpace.Lampion = newLampion;
+        newLampion.TravelTarget = targetSpace.transform.position;
+    }
+}
