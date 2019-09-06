5cc11165592016c2fbc63ec8e02e7520bb14ace3
Cornelius <Cornelius@localhost>
2019-09-06 02:16:22 +0200

    added context sensitve ui that shows you the hotkey if near removed oooooolllllddddd lampionplaceholders

---
+++ Assets/DreampostOffice/HotkeyDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotkeyDisplay : MonoBehaviour
+{
+    [SerializeField] private GameObject dreamPostOffice;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject floatingTextPrefab;
+
+    private GameObject hotkeyText;
+    private Vector3 offset = new Vector3(4,4, 0);
+
+
+    void Update()
+    {
+        var playerInRange = Vector3.Distance(dreamPostOffice.transform.position, player.transform.position);
+
+        if (playerInRange <= 10.0f)
+        {
+            if (!transform.GetComponentInChildren<TextMesh>())
+            {
+                hotkeyText = Instantiate(floatingTextPrefab, transform.position + offset, Quaternion.identity, transform);
+            }
+
+            hotkeyText.transform.LookAt(Camera.main.transform);
+            hotkeyText.transform.Rotate(0,180,0);
+        }
+        else if (hotkeyText != null)
+        {
+            Destroy(hotkeyText);
+        }
+    }
+}
